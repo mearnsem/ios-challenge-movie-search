4ca8312e880c261349a57cfd136df7f7a013bba7
Emily Mearns <mearnsem@gmail.com>
2016-06-10 09:17:20 -0600

    Storyboard set up, files created

---
+++ MovieSearch/MovieController.swift
@@ -0,0 +1,9 @@
+//
+//  MovieController.swift
+//  MovieSearch
+//
+//  Created by Emily Mearns on 6/10/16.
+//  Copyright © 2016 DevMountain. All rights reserved.
+//
+
+import Foundation
---
+++ MovieSearch/MovieTableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  MovieTableViewCell.swift
+//  MovieSearch
+//
+//  Created by Emily Mearns on 6/10/16.
+//  Copyright © 2016 DevMountain. All rights reserved.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var ratingLabel: UILabel!
+    @IBOutlet weak var summaryLabel: UILabel!
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+    func updateWithMovie(movie: Movie) {
+        
+    }
+
+}
